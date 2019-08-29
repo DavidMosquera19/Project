f53e48da7c2b7442ee23f9c5d8b1a20280c6fe34
DavidMosquera19 <53918702+DavidMosquera19@users.noreply.github.com>
2019-08-29 18:09:22 -0500

    secion de contacto

--- contacto.php
+++ contacto.php
@@ -4,27 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gym</title>
+    <title>Contacto</title>
+    <link rel="stylesheet" href="css/estilo2.css">
     <script src="https://kit.fontawesome.com/5faa3200c2.js"></script>
     <!-- sitilo de iconos -->
         <style>
-            body{
-                background: #fff;
-            }
+
             nav{
-                width: 150px;
-                height: 600px;
+                width: 55px;
+                height: 150px;
                 position: fixed;
             }
             nav a{
                 width: 100%;
-                height: 150px;
+                height: 50px;
                 color: #fff;
                 background: #333;
                 float: left;
                 font-size: 50px;
                 text-align: center;
-                line-height: 150px;
+                line-height: 50px;
                 border: 2px solid #000;
             }
             nav a:hover{
@@ -34,13 +33,18 @@
         </style>
 </head>
 <body>
-  
-    <nav>
-    
-        <a href="login.php"><i class="fas fa-user"></i></a>
-        <a href=""><i class="fas fa-comments"></i></a>
-        <a href="contacto.php"><i class="fas fa-address-book"></i></i></a>
+<nav>
+        <a href="index.php"><i class="fas fa-home"></i></a>
+       
     </nav>
-    
+    <form action="">
+    <h2>CONTACTO</h2>
+    <input type="text" nombre="Nombre" placeholder="Nombre">
+    <input type="text" nombre="Correo" placeholder="Correo">
+    <input type="text" nombre="Telefono" placeholder="Telefono">
+    <textarea name="Mensaje"placeholder="Escriban aqui el mensaje"></textarea>
+    <input type="button" value="Enviar" id="boton">
+    </form>
+
 </body>
 </html>
