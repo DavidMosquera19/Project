26336d8b76682833b5d79cbd7eb001a8b401302b
DavidMosquera19 <53918702+DavidMosquera19@users.noreply.github.com>
2019-11-14 16:09:58 -0500

    ..

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="css/estilo.css">
     <link rel="stylesheet" href="fonts.css">
+    <link rel="shortcut icon" href="imagen/indice.png" type="image/x-icon">
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="js/main.js"></script>
     <script src="https://kit.fontawesome.com/5faa3200c2.js"></script>
@@ -37,7 +38,11 @@
                  </ul>
              </nav>
              </center>
+             <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #08f;"></path></svg></div>
     </header>
-        
+        <main>
+            <section class="contenedor sobre-nosotros">
+            </section>
+        </main>
 </body>
 </html>
